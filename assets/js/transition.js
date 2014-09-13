d114901181c21d57aae76b5b9613dbe0ee692e73
DeathscytheH <alejandro.deathscythe@gmail.com>
2014-09-13 13:20:31 -0500

    F

--- assets/js/transition.js
+++ assets/js/transition.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
-  $("#cf7_controls").on('click', 'span', function() {
-    $("#cf7 img").removeClass("opaque");
 
-    var newImage = $(this).index();
+  $(document).ready(function() {
+    $("#cf7_controls").on('click', 'span', function() {
+      $("#cf7 img").removeClass("opaque");
 
-    $("#cf7 img").eq(newImage).addClass("opaque");
+      var newImage = $(this).index();
 
-    $("#cf7_controls span").removeClass("selected");
-    $(this).addClass("selected");
+      $("#cf7 img").eq(newImage).addClass("opaque");
+
+      $("#cf7_controls span").removeClass("selected");
+      $(this).addClass("selected");
+    });
   });
-});
